27d8a8792bc65049b0692a239afa52815f90e99b
changwoo Jeong <jcw9828@gmail.com>
2025-04-05 14:52:00 +0900

    [add] try catch

--- src/App.js
+++ src/App.js
@@ -27,10 +27,15 @@ function App() {
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    setWeather(data);
-    setLoading(false);
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const getWeatherByCity = async () => {
